c6cf914e9ac39b213f9ad68a9a0fafcdb1c1bce4
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-10-24 16:19:42 +0530

    some message

---
+++ mongodb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[30m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root privilege"
+    exit 1 #failure is other than 0
+fi
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+VALIDATE(){ #function receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    fi     
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo repo"
+
+dnf install mongodb-org -y &>>LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod &>>LOG_FILE
+VALIDATE $? "Start MongoDB"
+
