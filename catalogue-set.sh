afd33f6e07244ad9337fa5c3013f2f51acc6fd0e
Raghava siddartha Tripuraribhotla <raghavasiddarthatripuraribhotla@Raghavas-MacBook-Pro.local>
2025-11-02 16:59:00 +0530

    some message

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -17,20 +17,16 @@ SCRIPT_DIR=$PWD
 MONGODB_HOST="mongodb.ayaansh123.fun"
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root privilege"
     exit 1 #failure is other than 0
 fi
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at : $(date)" | tee -a $LOG_FILE
 
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    else
-        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
-    fi     
 
 ### NODEJS ###
 dnf module disable nodejs -y &>>LOG_FILE
