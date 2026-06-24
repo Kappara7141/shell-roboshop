3595052c02217e711600a42a0de3f846d5b85e50
sravya <ksravya66@gmail.com>
2026-06-24 15:36:58 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ DOMAIN_ID="ayaansh123.fun"
 
 for instance in @
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     #Get private Ip
     if [ $instance != "frontend" ];then
